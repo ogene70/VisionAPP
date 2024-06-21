65840b37aac7f7cb217b41d8e6b89bf1ffee284f
ogene70 <150164544+ogene70@users.noreply.github.com>
2024-06-21 11:51:48 +0200

    little changes

---
+++ vision-react/component/NavBar.js
@@ -0,0 +1,11 @@
+import React,{useState,useEffect,StyleHTMLAttributes} from "react";
+
+function NavBar(props){
+    return (
+        <div id="NavBar">
+            <img src="" alt="ESCANDE BALZAN SARL LOGO"></img>
+        </div>
+    );
+}
+
+export default NavBar;
